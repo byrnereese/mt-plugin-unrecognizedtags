f5cdbc60fd021463007730cc7bcde99124e69af4
Finn Smith <finn@timeghost.net>
2010-10-07 16:01:41 -0400

    Fix whitespace.

--- plugins/UnrecognizedTags/lib/UnrecognizedTags/CMS.pm
+++ plugins/UnrecognizedTags/lib/UnrecognizedTags/CMS.pm
@@ -1,13 +1,13 @@
 #############################################################################
 # Copyright © 2008-2009 Six Apart Ltd.
-# This program is free software: you can redistribute it and/or modify it 
+# This program is free software: you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License as published
-# by the Free Software Foundation, or (at your option) any later version.  
+# by the Free Software Foundation, or (at your option) any later version.
 # This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
-# version 2 for more details.  You should have received a copy of the GNU 
-# General Public License version 2 along with this program. If not, see 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details.  You should have received a copy of the GNU
+# General Public License version 2 along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
 
 package UnrecognizedTags::CMS;
@@ -69,7 +69,7 @@ sub unrecognized_tags {
 		'tmpl_loop' => \@tmpl_loop,
 		 });
 	}
-	my $param = { 
+	my $param = {
 	    'tag_count' => $tag_count,
 	    'instance_count' => $instance_count,
 	    'tag_loop' => \@tag_loop,
